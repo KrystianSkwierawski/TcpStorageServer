fba0a6e2143bdc912080103cd7d2aeb6aa9727ee
bezi25251@gmail.com <bezi25251@gmail.com>
2025-05-05 18:51:32 +0200

    Handle concurrent clients

--- src/Server.cs
+++ src/Server.cs
@@ -2,15 +2,30 @@ using System.Net;
 using System.Net.Sockets;
 using codecrafters_redis.src;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var socket = await server.AcceptSocketAsync(); // wait for client
 
-while (socket.Connected)
+Console.WriteLine("Server started on port 6379");
+
+var clients = new List<Task>();
+
+while (true)
 {
-    await socket.SendMessageAsync("PONG\r\n");
+    var socket = await server.AcceptSocketAsync();
+    Console.WriteLine("New client connected");
+
+    clients.Add(HandleClientAsync(socket));
 }
 
+async Task HandleClientAsync(Socket socket)
+{
+    using (socket)
+    {
+        while (socket.Connected)
+        {
+            await socket.SendMessageAsync("PONG\r\n");
+        }
+    }
+
+    Console.WriteLine("Client disconnected");
+}
